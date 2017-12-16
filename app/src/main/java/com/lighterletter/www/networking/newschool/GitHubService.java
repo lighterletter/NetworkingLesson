8fe40a54c0f82f35395d13e95b2d55666d7eba69
lighterletter <jgomez818@gmail.com>
2017-12-16 12:08:42 -0500

    updated to add path call

--- app/src/main/java/com/lighterletter/www/networking/newschool/GitHubService.java
+++ app/src/main/java/com/lighterletter/www/networking/newschool/GitHubService.java
@@ -4,11 +4,18 @@ import com.lighterletter.www.networking.model.Issue;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 
 public interface GitHubService {
+
     @GET("repos/rails/rails/issues")
-    Call<List<Issue>> getIssues();
+    Call<List<Issue>> getRailsIssues();
+
+    @GET("users/{username}/received_events")
+    Call<ResponseBody> getReceivedEvents(@Path("username") String username);
+
 }
--- app/src/main/java/com/lighterletter/www/networking/newschool/MainActivity.java
+++ app/src/main/java/com/lighterletter/www/networking/newschool/MainActivity.java
@@ -8,8 +8,10 @@ import android.util.Log;
 import com.lighterletter.www.networking.R;
 import com.lighterletter.www.networking.model.Issue;
 
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -20,9 +22,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by john on 12/15/17.
  */
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private static String TAG  = MainActivity.class.getSimpleName();
+    private static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,18 +38,40 @@ public class MainActivity extends AppCompatActivity{
                 .build();
 
         GitHubService service = retrofit.create(GitHubService.class);
-        Call<List<Issue>> issues = service.getIssues();
+        Call<List<Issue>> issues = service.getRailsIssues();
+
         issues.enqueue(new Callback<List<Issue>>() {
             @Override
             public void onResponse(Call<List<Issue>> call, Response<List<Issue>> response) {
-                Log.d(TAG, "onResponse: " + response.body().size());
+
+                List<Issue> issueList = response.body();
+
+                Log.d(TAG, "onResponse: " + issueList.get(0).getUser().getLogin());
             }
 
             @Override
             public void onFailure(Call<List<Issue>> call, Throwable t) {
-                Log.d(TAG, "onFailure: "+ t.toString());
+                Log.d(TAG, "onFailure: " + t.toString());
             }
         });
 
+
+        Call<ResponseBody> events = service.getReceivedEvents("y-yagi"); // using parameters passed in to the call
+        events.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.d(TAG, "onResponse: events: " + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.toString());
+
+            }
+        });
     }
 }
